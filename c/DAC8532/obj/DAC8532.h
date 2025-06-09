aaf60662b5a5a5ddeca972194548f40517897e9f
ZoOtMcNoOt <gmcnatt1@gmail.com>
2025-06-09 16:41:27 -0500

    Update DAC8532.h to set DAC_VREF to 5.0 and add ads1256_test file

--- c/DAC8532/obj/DAC8532.h
+++ c/DAC8532/obj/DAC8532.h
@@ -6,7 +6,7 @@
 #define channel_A   0x30
 #define channel_B   0x34
 #define DAC_Value_MAX  65535
-#define DAC_VREF  3.3
+#define DAC_VREF  5.0
 
 void DAC8532_Out_Voltage(UBYTE Channel, float Voltage);
 void Write_DAC8532(UBYTE Channel, UWORD Data);
